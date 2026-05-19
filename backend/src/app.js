f1caafa5ac73b1e3b7f131a1b371aa7a50af29c1
Divyansh Garg <divyanshgarg@Divyanshs-MacBook-Pro.local>
2026-05-19 23:39:41 +0530

    Add basic auth files and directory structure

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 import express from "express";
-import userRoutes from "./routes/userRoutes.js";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.json({
