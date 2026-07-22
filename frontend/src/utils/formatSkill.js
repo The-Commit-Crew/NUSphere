2f83b7cf76dd914ceb3a8a7bfd8ec9354ddbf98e
khushi46-dev <madnani.khushi46@gmail.com>
2026-07-23 01:25:17 +0530

    fix: changed skills to uppercase

--- frontend/src/utils/formatSkill.js
+++ frontend/src/utils/formatSkill.js
@@ -1,7 +1,7 @@
 export function formatSkill(name) {
   if (!name) return ''
   return name
-    .toLowerCase()
+    .toUpperCase()
     .split(' ')
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(' ')
